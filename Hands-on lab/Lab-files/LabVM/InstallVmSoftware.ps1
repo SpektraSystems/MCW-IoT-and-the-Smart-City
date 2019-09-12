f9e23d3147b1d470b46d2dd8c5351f8d672b3166
Carey Payette <carey.payette@gmail.com>
2019-09-12 14:45:53 -0400

    in progress update of labvm

---
+++ Hands-on lab/Lab-files/LabVM/InstallVmSoftware.ps1
@@ -0,0 +1,59 @@
+#Fix RDP Issue
+$HKLM = "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp"
+Set-ItemProperty -Path $HKLM -Name "SecurityLayer" -Value 0
+
+#Enable Containers
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All -All -NoRestart
+Enable-WindowsOptionalFeature -Online -FeatureName Containers -All -NoRestart
+
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Assign Packages to Install
+$Packages = 'googlechrome',`
+            'docker-for-windows',`
+            'postman',`
+            'nodejs.install',`
+            'vscode',`
+            'git',`
+            'visualstudio2019community',`
+            'visualstudio2019-workload-azure',`
+            'visualstudio2019-workload-manageddesktop',`
+            'visualstudio2019-workload-netweb',`
+            'dotnetcore-sdk'
+
+#Install Packages
+ForEach ($PackageName in $Packages)
+{choco install $PackageName -y}
+
+<#Update Visual Studio
+$command1 = @'
+cmd.exe /C C:\Users\demouser\AppData\Local\Temp\chocolatey\visualstudio2017community\15.2.26430.20170605\vs_community.exe --update --quiet --wait
+'@
+$command2 = @'
+cmd.exe /C C:\Users\demouser\AppData\Local\Temp\chocolatey\visualstudio2017community\15.2.26430.20170605\vs_community.exe --update --quiet --wait --passive --norestart --installPath `
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community"
+'@
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-Expression -Command:$command1
+Invoke-Expression -Command:$command2
+#>
+
+#Install Visual Studio Code Extensions
+Set-ExecutionPolicy Bypass -Scope Process -Force; & 'C:\Program Files\Microsoft VS Code\bin\code.cmd' --install-extension ms-vscode.csharp
+Set-ExecutionPolicy Bypass -Scope Process -Force; & 'C:\Program Files\Microsoft VS Code\bin\code.cmd' --install-extension vsciot-vscode.azure-iot-edge
+
+#Add Demo User to docker group
+Add-LocalGroupMember -Member demouser -Group docker-users
+
+#Bring down Desktop Shortcuts
+$zipDownload = "https://github.com/solliancenet/LABVM/blob/master/iotforbiz/shortcuts.zip?raw=true"
+$downloadedFile = "D:\shortcuts.zip"
+$vmFolder = "C:\Users\Public\Desktop"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest $zipDownload -OutFile $downloadedFile
+Add-Type -assembly "system.io.compression.filesystem"
+[io.compression.zipfile]::ExtractToDirectory($downloadedFile, $vmFolder)
+
+#Reboot
+Restart-Computer
---
+++ Hands-on lab/Lab-files/LabVM/README.md
@@ -0,0 +1,17 @@
+# Lab Virtual Machine Deployment
+If you would like to deploy a fully configured Windows 10 based virtual machine in Azure to complete this lab, press the button below.
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fsolliancenet%2Flabvm%2Fmaster%2Fiotforbiz%2Fazure-deploy.json" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+
+
+
+**Prerequisites Supplied:**
+
+1. Docker for Windows Community Edition
+1. Visual Studio Community 2019 with Updates (.NET Desktop Environment, ASP and Web Development and Azure SDK)
+1. Node.js
+1. Postman
+1. Git for Windows (bash)
+1. Visual Studio Code (IoT Edge, Omnisharp CSharp)
